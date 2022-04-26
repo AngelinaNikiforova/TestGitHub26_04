bd1181a8305d2d5ba5afe0265f3a366631429de8
Odink923 <odink923@gmail.com>
2022-04-26 20:42:28 +0300

    авававав

--- 26_04/Program.cs
+++ 26_04/Program.cs
@@ -6,7 +6,7 @@ namespace _26_04
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello People!");
+            Console.WriteLine("Hello People4!");
             Console.WriteLine("Hello!");
             Console.WriteLine("Hiel");
         }
